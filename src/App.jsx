104a0adde9d5efe5df77da25c707fa955dc5f9e8
valentina <valentina.leitaop@gmail.com>
2024-08-15 14:54:40 -0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Home from './Home'
+import '../styles/App.css'
+import Informacion from './Informacion.jsx'
+import Beneficios from './Beneficios.jsx'
+import Contacto from './Contacto.jsx'
+import Reserva from './Reserva.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <Home/>
+      <Informacion/>
+      <Beneficios/>
+      <Contacto/>
+      <Reserva/>
+
+
+
+    </>
+  )
+}
+
+export default App
